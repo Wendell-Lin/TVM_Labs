2700427a68fd88f2197b5c6a12e6444ceca4af72
Wendell-Lin <s950085az@gmail.com>
2022-08-09 14:46:46 +0800

    test

---
+++ C_API/main.cpp
@@ -0,0 +1,19 @@
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <functional>
+#include <dlpack/dlpack.h>
+#include <tvm/runtime/object.h>
+#include <tvm/runtime/module.h>
+#include <tvm/runtime/registry.h>
+#include <tvm/runtime/packed_func.h>
+
+int main(){
+std::cout<<"hellow, tvm\n";
+    return 0;
+}
+
